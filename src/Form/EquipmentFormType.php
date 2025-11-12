94a33a1e354111ca0b1a7a7bef37969be05cf993
Vincent Bragard <github.requisite986@passmail.net>
2025-11-12 16:45:19 +0100

    fix: on enlève le droit de modifier le type d'un équipement après sa création

--- src/Form/EquipmentFormType.php
+++ src/Form/EquipmentFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Club;
 use App\Entity\Equipment;
 use App\Entity\User;
+use App\Entity\Yumi;
 use App\Enum\EquipmentType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -21,19 +22,11 @@ class EquipmentFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('equipment_type', EnumType::class, [
-                'class' => EquipmentType::class,
-                'choice_label' => fn(EquipmentType $type) => $type->label(),
-                'label' => 'Type d\'équipement',
-                'placeholder' => 'equipment.choose_type',
-                'translation_domain' => 'messages',
-                'mapped' => false,
-            ])
             ->add('owner_club', EntityType::class, [
                 'class' => Club::class,
                 'choice_label' => 'name',
                 'placeholder' => '--- Aucun ---',
-                'required' => false,
+                'required' => true,
             ])
             ->add('borrower_club', EntityType::class, [
                 'class' => Club::class,
@@ -44,24 +37,34 @@ class EquipmentFormType extends AbstractType
             ->add('borrower_user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'email',
+                'placeholder' => '--- Aucun ---',
+                'required' => false
             ])
             ->add('save', SubmitType::class, ['label' => 'Envoyer'])
         ;
 
-        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
             $form = $event->getForm();
             $data = $event->getData();
 
-            if ($data instanceof Equipment) {
-                $form->get('equipment_type')->setData($data::getType());
+            if (empty($data)) {
+                $form
+                    ->add('equipment_type', EnumType::class, [
+                        'class' => EquipmentType::class,
+                        'choice_label' => fn(EquipmentType $type) => $type->label(),
+                        'label' => 'Type d\'équipement',
+                        'placeholder' => 'equipment.choose_type',
+                        'translation_domain' => 'messages',
+                        'mapped' => false,
+                        'required' => true,
+                    ])
+                ;
             }
         });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Equipment::class,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
