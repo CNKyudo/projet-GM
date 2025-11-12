37a1536de38dd662f043034ab09512e031f5f117
Vincent Bragard <github.requisite986@passmail.net>
2025-11-12 20:22:20 +0100

    fix: retrait de l'import du js sur la modification des équipements

--- assets/equipment_form.js
+++ assets/equipment_form.js
@@ -1,12 +1,10 @@
 import {disableFormSection, enableFormSection} from "./form_utils.js";
 
-'./form_utils.js'
-
 const glove_form_section = document.getElementById('glove_form_section')
 const yumi_form_section = document.getElementById('yumi_form_section')
 
 document.getElementById('equipment_form_equipment_type')
-    .addEventListener('change', function (e) {
+    .addEventListener('change', function () {
         if (this.value !== 'glove') {
             disableFormSection(glove_form_section)
         } else {
