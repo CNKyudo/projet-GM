83c68cc9c77b210ea35d5e611c429579fc1342bd
Vincent Bragard <github.requisite986@passmail.net>
2026-04-29 12:28:53 +0200

    refactor: ordre des queries dans la migration

--- migrations/Version20260428162253.php
+++ migrations/Version20260428162253.php
@@ -20,11 +20,11 @@ final class Version20260428162253 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipment ADD height DOUBLE PRECISION DEFAULT NULL');
         $this->addSql('DROP INDEX idx_75ea56e016ba31db');
         $this->addSql('DROP INDEX idx_75ea56e0e3bd61ce');
         $this->addSql('DROP INDEX idx_75ea56e0fb7336f0');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (queue_name, available_at, delivered_at, id)');
+        $this->addSql('ALTER TABLE equipment ADD height DOUBLE PRECISION DEFAULT NULL');
         $this->addSql('ALTER TABLE equipment ADD nb_bows INT DEFAULT NULL');
         $this->addSql('ALTER TABLE equipment ADD orientation VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE equipment ADD nb_arrows INT DEFAULT NULL');
@@ -38,11 +38,11 @@ final class Version20260428162253 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipment DROP height');
         $this->addSql('DROP INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750');
         $this->addSql('CREATE INDEX idx_75ea56e016ba31db ON messenger_messages (delivered_at)');
         $this->addSql('CREATE INDEX idx_75ea56e0e3bd61ce ON messenger_messages (available_at)');
         $this->addSql('CREATE INDEX idx_75ea56e0fb7336f0 ON messenger_messages (queue_name)');
+        $this->addSql('ALTER TABLE equipment DROP height');
         $this->addSql('ALTER TABLE equipment DROP nb_bows');
         $this->addSql('ALTER TABLE equipment DROP orientation');
         $this->addSql('ALTER TABLE equipment DROP nb_arrows');
