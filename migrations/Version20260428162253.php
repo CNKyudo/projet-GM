4fd61b9679a0bb93fa43e4979d344f10e6a4882b
Vincent Bragard <github.requisite986@passmail.net>
2026-04-29 12:00:14 +0200

    refactor: fusion de toutes les migrations de la R en une seule

--- migrations/Version20260428162253.php
+++ migrations/Version20260428162253.php
@@ -20,20 +20,36 @@ final class Version20260428162253 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipment ADD hauteur DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD height DOUBLE PRECISION DEFAULT NULL');
         $this->addSql('DROP INDEX idx_75ea56e016ba31db');
         $this->addSql('DROP INDEX idx_75ea56e0e3bd61ce');
         $this->addSql('DROP INDEX idx_75ea56e0fb7336f0');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (queue_name, available_at, delivered_at, id)');
+        $this->addSql('ALTER TABLE equipment ADD nb_arcs INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD orientation VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD nb_fleches INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD length DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD weight DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD accroche VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD width DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE equipment ADD thickness DOUBLE PRECISION DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE equipment DROP hauteur');
+        $this->addSql('ALTER TABLE equipment DROP height');
         $this->addSql('DROP INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750');
         $this->addSql('CREATE INDEX idx_75ea56e016ba31db ON messenger_messages (delivered_at)');
         $this->addSql('CREATE INDEX idx_75ea56e0e3bd61ce ON messenger_messages (available_at)');
         $this->addSql('CREATE INDEX idx_75ea56e0fb7336f0 ON messenger_messages (queue_name)');
+        $this->addSql('ALTER TABLE equipment DROP nb_arcs');
+        $this->addSql('ALTER TABLE equipment DROP orientation');
+        $this->addSql('ALTER TABLE equipment DROP nb_fleches');
+        $this->addSql('ALTER TABLE equipment DROP length');
+        $this->addSql('ALTER TABLE equipment DROP weight');
+        $this->addSql('ALTER TABLE equipment DROP accroche');
+        $this->addSql('ALTER TABLE equipment DROP width');
+        $this->addSql('ALTER TABLE equipment DROP thickness');
     }
 }
