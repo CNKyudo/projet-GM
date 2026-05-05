19d71ccf1f1cbe9ffbd83aaaf68d1757bb5d6614
Vincent Bragard <github.requisite986@passmail.net>
2026-05-05 22:32:12 +0200

    fix: cleanup turbo non utilisé

--- importmap.php
+++ importmap.php
@@ -28,13 +28,10 @@ return [
         'path' => './assets/flash_toasts.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
     ],
 ];
